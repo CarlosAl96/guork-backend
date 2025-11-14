8b5f1916e1494ae37501329edbf3c5b370228f2b
Carlos Ceballo <carlos96cacv@gmail.com>
2025-11-14 18:02:16 -0400

    refactor: remove database connection logic from serverless handler

--- dist/vercel.js
+++ dist/vercel.js
@@ -6,20 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = handler;
 const serverless_http_1 = __importDefault(require("serverless-http"));
 const app_1 = __importDefault(require("./app"));
-const database_1 = __importDefault(require("./config/database"));
 const serverlessHandler = (0, serverless_http_1.default)(app_1.default);
-let sequelizeReady = null;
-const ensureDbConnection = () => {
-    if (!sequelizeReady) {
-        sequelizeReady = database_1.default.authenticate().catch((error) => {
-            console.error("❌ Unable to connect to database:", error);
-            sequelizeReady = null;
-            throw error;
-        });
-    }
-    return sequelizeReady;
-};
 async function handler(req, res) {
-    await ensureDbConnection();
     return serverlessHandler(req, res);
 }
--- src/vercel.ts
+++ src/vercel.ts
@@ -1,23 +1,8 @@
 import serverless from "serverless-http";
 import app from "./app";
-import sequelize from "./config/database";
 
 const serverlessHandler = serverless(app);
 
-let sequelizeReady: Promise<void> | null = null;
-
-const ensureDbConnection = () => {
-	if (!sequelizeReady) {
-		sequelizeReady = sequelize.authenticate().catch((error) => {
-			console.error("❌ Unable to connect to database:", error);
-			sequelizeReady = null;
-			throw error;
-		});
-	}
-	return sequelizeReady;
-};
-
 export default async function handler(req: any, res: any) {
-	await ensureDbConnection();
-	return serverlessHandler(req, res);
+  return serverlessHandler(req, res);
 }
